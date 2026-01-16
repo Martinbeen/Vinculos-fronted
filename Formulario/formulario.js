a5dfa5faf2fbd1236a1015ec860c750b67457ea9
Martin <miorgullo926@gmail.com>
2026-01-16 17:38:37 -0500

    Subida inicial del fronted

---
+++ Formulario/formulario.js
@@ -0,0 +1,36 @@
+const user = document.getElementById("usuario");
+const pass = document.getElementById("password");
+const iniciar_sesion = document.getElementById("iniciar_sesion");
+
+function ValidarUsuario(user, pass, iniciar_sesion){
+    iniciar_sesion.addEventListener("click", () => {
+        //event.preventDefault(); // evitar que el form recargue la página
+        if (user.value.length === 0 || pass.value.length === 0) return;
+
+        const login = {NombreUsuario: user.value, Password: pass.value}
+
+        fetch("https://vinculos-backend-fth6etbkfhfwbqhn.centralus-01.azurewebsites.net", {
+            method: "Post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(login)
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.existe){
+                alert(response.json());
+                window.location.href = "";
+            }
+            else{
+                alert("Creadenciales invalidas: " + data.mensaje);
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("Error de conexión con el servidor");
+        })
+    })
+}
+
+ValidarUsuario(user, pass, iniciar_sesion);
