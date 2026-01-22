5e91ccf76f1a69202cc18455f7bcb4158ee996fb
Martin <miorgullo926@gmail.com>
2026-01-21 20:22:58 -0500

    cambio

--- formulario.js
+++ formulario.js
@@ -21,7 +21,7 @@ function ValidarUsuario(user, pass, iniciar_sesion){
         .then(data => {
             if (data.existe){
                 alert("Entrada Correcta");
-                //window.location.href = ""
+                window.location.href = "/Contenido/contenido.html"
             }
             else{
                 alert("Credenciales invalidas: " + data.mensaje);
